7864108a1bde3ac0a58cd75dcc64e61671a66f60
Kim._.jazz <jeong011010@naver.com>
2022-02-10 02:02:21 +0900

    update js file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+// 중괄호로 데이터 바인딩이 가능.
+let posts = '강남 고기 맛짐';
+function 함수(){
+  return 100
+}
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
+      <img src={ logo } />
+      <h4> { posts } </h4>
     </div>
   );
 }
