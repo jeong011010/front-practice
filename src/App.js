7835b858ff1e08ce36d1638c7b269a944c27cbae
Kim._.jazz <jeong011010@naver.com>
2022-02-16 22:11:16 +0900

    new project [To do 웹페이지 만들기]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import './App.css';
-import Nav from './component/Nav/Nav';
-import Startpage from './component/Startpage/Startpage';
-import {BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Mainpage from './component/Mainpage/Main';
-import Finishpage from './component/Finishpage/Finish';
-import React, {component, useState} from "react";
+import React from "react";
+import styled from "styled-components";
 
-function App(){
+class App extends React.Component {
+  state = {
+    query:""
+  };
 
-  return(
-    <BrowserRouter>
-      <div className='App'>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Startpage/>}/>
-          <Route path="/main" element={<Mainpage/>}/>
-          <Route path="/finish" element={<Finishpage/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+  render() {
+    return (
+      <Container query={this.state.query}>
+        <Input
+          placeholder="테마를 입력하세요"
+          onKeyPress={this.handleInputKeyPress}
+        ></Input>
+      </Container>
+    );
+  }
+
+  handleInputKeyPress = event => {
+    if (event.key === "Enter") {
+      this.setState({
+        query: event.target.value
+      });
+      event.target.value="";
+    }
+  };
 }
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+      to right,
+      rgba(20, 20, 20, 0.1) 10%,
+      rgba(20, 20, 20, 0.7) 70%,
+      rgba(20, 20, 20, 1)
+    ),url(https://source.unsplash.com/random/1920x1080?${props => props.query});
+  background-size: cover;
+`;
+
+const Input = styled.input`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 190px;
+  height: 33px;
+  padding: 3px;
+  background: transparent;
+  outline: none;
+  border: none;
+  font-size: 22px;
+  color: white;
+`;
 
 export default App;
